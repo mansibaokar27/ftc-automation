08d0bfda511e337d9d93d70030ebcc3a6cfafd72
mansi.baokar <mansi.baokar@RAV-DSK-339>
2018-01-30 17:40:52 +0530

    initial commit

---
+++ FTC.Framework/PageObject/PageObjectDashboard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using NUnit;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FTC.Framework.PageObject
+{
+   public class PageObjectDashboard
+    {
+        private IWebDriver driver;
+        private IWait<OpenQA.Selenium.IWebDriver> iWait;
+
+        public PageObjectDashboard(IWebDriver driver, IWait<OpenQA.Selenium.IWebDriver> iWait)
+        {
+            this.driver = driver;
+            this.iWait = iWait;
+        }
+
+        #region Elements
+
+        By profile = By.CssSelector("button.border-none.mat-button");
+
+        #endregion
+        public void selectProfile()
+        {
+            driver.FindElement(profile).Click();
+            driver.FindElements(By.CssSelector("div.mat-menu-ripple.mat-ripple"))[0].Click();
+            //Random ran = new Random();
+            //int temp = ran.Next(0, 1000);
+            //string name = "Marvel" + temp;
+            //driver.SwitchTo().Frame("ssIFrame_google");
+
+           // driver.SwitchTo().DefaultContent();
+        }
+        public void verifylogin()
+        {
+            Assert.AreEqual( true, driver.FindElement(By.LinkText("Dashboard")).Enabled);
+        }
+    }
+}
